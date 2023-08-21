ed346771fb1c2bd576115b43065b7d879dea2689
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-20 23:56:59 -0500

    add

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import streamlit as st
-
-# Configuración de la página
-st.set_page_config(page_title="Dashboard de Datos", layout="wide")
-
-# Título de la página
-st.title("Dashboard de Datos")
 
 
 # Ejecutar la aplicación
 if __name__ == "__main__":
-    st.run()
+    # Configuración de la página
+    st.set_page_config(page_title="Dashboard de Datos", layout="wide")
+
+    # Título de la página
+    st.title("Dashboard de Datos")
+    
