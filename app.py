12abd70943f2ce14755171939c883bb4471a9c2c
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-21 01:15:56 -0500

    add

--- app.py
+++ app.py
@@ -1,4 +1,43 @@
 import streamlit as st
+
+# Estilos CSS personalizados para la barra de menú
+custom_styles = """
+<style>
+.navbar {
+    display: flex;
+    background-color: #333;
+    padding: 10px 0;
+    color: white;
+}
+
+.nav-link {
+    text-decoration: none;
+    color: white;
+    padding: 5px 10px;
+}
+
+.nav-link:hover {
+    background-color: #555;
+}
+</style>
+"""
+
+# Barra de menú personalizada
+st.markdown(custom_styles, unsafe_allow_html=True)
+menu_option = st.selectbox("", ["Inicio", "Fin"], key="menu")
+
+# Sección de contenido
+st.title("Contenido")
+
+# Agrega aquí tu lógica para mostrar el contenido correspondiente a la opción seleccionada
+if menu_option == "Inicio":
+    st.write("Hola, bienvenido.")
+elif menu_option == "Fin":
+    st.write("Adiós, hasta luego.")
+
+
+"""
+import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -147,3 +186,5 @@ else:
 #st.sidebar.title("Contacto")
 #st.sidebar.write("Correo electrónico: ejemplo@example.com")
 #st.sidebar.write("Teléfono: +123-456-7890")
+
+"""
