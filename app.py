8a91f33f8792d24ffafc835894f20d3ea4a1b54a
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-21 01:23:27 -0500

    add

--- app.py
+++ app.py
@@ -1,15 +1,48 @@
 import streamlit as st
 
-# Crear una función para mostrar el contenido según la opción seleccionada
-def show_content(option):
-    if option == "Inicio":
-        st.write("Contenido de Inicio: ¡Hola!")
-    elif option == "Fin":
-        st.write("Contenido de Fin: ¡Adiós!")
+# Agregar el estilo CSS personalizado para el menú en el header
+st.markdown(
+    """
+    <style>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        background-color: #f2f2f2;
+        padding: 10px 20px;
+        align-items: center;
+    }
+    
+    .menu-item {
+        margin-right: 20px;
+        color: #333;
+        text-decoration: none;
+        font-weight: bold;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-# Crear una barra de menú en la barra lateral
-st.sidebar.title("Menú")
-menu_option = st.sidebar.radio("Selecciona una opción:", ["Inicio", "Fin"])
+# Crear el menú en el header
+st.markdown(
+    """
+    <div class="header">
+        <a class="menu-item" href="#">Inicio</a>
+        <a class="menu-item" href="#">Opción 1</a>
+        <a class="menu-item" href="#">Opción 2</a>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
 
-# Mostrar el contenido según la opción seleccionada
-show_content(menu_option)
+# Contenido de la página
+st.title("Contenido de la Página")
+
+# Detectar la opción seleccionada en el menú
+menu_option = st.session_state.selected_option
+
+# Mostrar el contenido correspondiente a la opción seleccionada
+if menu_option == "Opción 1":
+    st.write("Hola")
+elif menu_option == "Opción 2":
+    st.write("Adiós")
