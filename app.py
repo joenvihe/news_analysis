05d15112ff1e621aaf4cf66e43abff699da0fd4b
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-21 01:20:12 -0500

    add

--- app.py
+++ app.py
@@ -1,36 +1,15 @@
 import streamlit as st
 
-# Estilos CSS personalizados para la barra de menú
-custom_styles = """
-<style>
-.navbar {
-    display: flex;
-    background-color: #333;
-    padding: 10px 0;
-    color: white;
-}
+# Crear una función para mostrar el contenido según la opción seleccionada
+def show_content(option):
+    if option == "Inicio":
+        st.write("Contenido de Inicio: ¡Hola!")
+    elif option == "Fin":
+        st.write("Contenido de Fin: ¡Adiós!")
 
-.nav-link {
-    text-decoration: none;
-    color: white;
-    padding: 5px 10px;
-}
+# Crear una barra de menú en la barra lateral
+st.sidebar.title("Menú")
+menu_option = st.sidebar.radio("Selecciona una opción:", ["Inicio", "Fin"])
 
-.nav-link:hover {
-    background-color: #555;
-}
-</style>
-"""
-
-# Barra de menú personalizada
-st.markdown(custom_styles, unsafe_allow_html=True)
-menu_option = st.selectbox("", ["Inicio", "Fin"], key="menu")
-
-# Sección de contenido
-st.title("Contenido")
-
-# Agrega aquí tu lógica para mostrar el contenido correspondiente a la opción seleccionada
-if menu_option == "Inicio":
-    st.write("Hola, bienvenido.")
-elif menu_option == "Fin":
-    st.write("Adiós, hasta luego.")
+# Mostrar el contenido según la opción seleccionada
+show_content(menu_option)
