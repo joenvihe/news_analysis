0a83eb5c2624d3a14cd8d63dc7d27e4880b82650
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-21 00:06:52 -0500

    add

--- app.py
+++ app.py
@@ -15,40 +15,48 @@ data = pd.DataFrame({
 # Título de la página
 st.title("Dashboard de Análisis de Datos")
 
-# Gráfico de líneas para Ventas y Gastos
-st.subheader("Gráfico de Ventas y Gastos")
-plt.figure(figsize=(10, 6))
-sns.lineplot(data=data, x='Fecha', y='Ventas', label='Ventas')
-sns.lineplot(data=data, x='Fecha', y='Gastos', label='Gastos')
-plt.xlabel("Fecha")
-plt.ylabel("Monto")
-plt.legend()
-st.pyplot(plt)
+# Create two columns layout
+col1, col2 = st.beta_columns(2)
 
-# Gráfico de barras para Utilidad
-st.subheader("Gráfico de Utilidad")
-plt.figure(figsize=(10, 6))
-sns.barplot(data=data, x='Fecha', y='Utilidad')
-plt.xlabel("Fecha")
-plt.ylabel("Utilidad")
-plt.xticks(rotation=45)
-st.pyplot(plt)
+# Column 1: Display country data
+with col1:
+    # Gráfico de líneas para Ventas y Gastos
+    st.subheader("Gráfico de Ventas y Gastos")
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(data=data, x='Fecha', y='Ventas', label='Ventas')
+    sns.lineplot(data=data, x='Fecha', y='Gastos', label='Gastos')
+    plt.xlabel("Fecha")
+    plt.ylabel("Monto")
+    plt.legend()
+    st.pyplot(plt)
 
-# Gráfico de dispersión para Ventas vs. Utilidad
-st.subheader("Gráfico de Ventas vs. Utilidad")
-plt.figure(figsize=(10, 6))
-sns.scatterplot(data=data, x='Ventas', y='Utilidad')
-plt.xlabel("Ventas")
-plt.ylabel("Utilidad")
-st.pyplot(plt)
 
-# Tabla de datos
-st.subheader("Tabla de Datos")
-st.dataframe(data)
+    # Gráfico de dispersión para Ventas vs. Utilidad
+    st.subheader("Gráfico de Ventas vs. Utilidad")
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(data=data, x='Ventas', y='Utilidad')
+    plt.xlabel("Ventas")
+    plt.ylabel("Utilidad")
+    st.pyplot(plt)
 
-# Resumen estadístico
-st.subheader("Resumen Estadístico")
-st.write(data.describe())
+# Column 2: Display charts
+with col2:
+    # Gráfico de barras para Utilidad
+    st.subheader("Gráfico de Utilidad")
+    plt.figure(figsize=(10, 6))
+    sns.barplot(data=data, x='Fecha', y='Utilidad')
+    plt.xlabel("Fecha")
+    plt.ylabel("Utilidad")
+    plt.xticks(rotation=45)
+    st.pyplot(plt)
+
+    # Tabla de datos
+    st.subheader("Tabla de Datos")
+    st.dataframe(data)
+
+    # Resumen estadístico
+    st.subheader("Resumen Estadístico")
+    st.write(data.describe())
 
 # Información de contacto
 st.sidebar.title("Contacto")
