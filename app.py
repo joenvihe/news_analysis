be696cbedd930370154bd436b198863f36224fe7
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-20 23:54:29 -0500

    add

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Import data
-df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+import streamlit as st
 
-# Create sidebar
-st.sidebar.title("COVID-19 Data")
+# Configuración de la página
+st.set_page_config(page_title="Dashboard de Datos", layout="wide")
 
-# Select country
-country = st.sidebar.selectbox("Country", df["location"].unique())
+# Título de la página
+st.title("Dashboard de Datos")
 
-# Display data
-st.write(df[df["location"] == country])
 
 # Ejecutar la aplicación
 if __name__ == "__main__":
