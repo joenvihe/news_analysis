08dd82f23a177438d2c19a29df1375609f666fc7
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-25 01:24:36 -0500

    add

--- app.py
+++ app.py
@@ -210,9 +210,17 @@ if option == "inicio":
         # Gráfico de dispersión para Ventas vs. Utilidad
         st.subheader("Gráfico de Ventas vs. Utilidad")
         plt.figure(figsize=(10, 6))
-        sns.scatterplot(data=data, x='Ventas', y='Utilidad')
-        plt.xlabel("Ventas")
-        plt.ylabel("Utilidad")
+        sns.set_theme(style="whitegrid")
+        # Load the diamonds dataset
+        diamonds = sns.load_dataset("diamonds")
+        # Plot the distribution of clarity ratings, conditional on carat
+        sns.displot(
+            data=diamonds,
+            x="carat", hue="cut",
+            kind="kde", height=6,
+            multiple="fill", clip=(0, None),
+            palette="ch:rot=-.25,hue=1,light=.75",
+        )
         st.pyplot(plt)
 
     # Column 2: Display charts
